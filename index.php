a3c48d29867981dc435ca6eaa830df46dc0f1bf7
harpreet <Harpreet Kour>
2020-08-17 13:59:52 -0600

    build theme

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  /**
+   * The get_header() function will grab and run our 'header.php' file for this theme.
+   */
+  get_header();
+?>
+
+<p>This is index.php</p>
+
+<?php
+  /**
+   * The get_footer() function will grab and run our 'footer.php' file for this theme.
+   */
+  get_footer();
